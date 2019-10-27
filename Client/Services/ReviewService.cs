72be33f2fcbd9f2d5296146f5d3d730572bf7ef7
Stephen Bakalian <stephen.bakalian@034gc.onmicrosoft.com>
2019-10-27 19:59:05 -0400

    implement getters in service

--- Client/Services/ReviewService.cs
+++ Client/Services/ReviewService.cs
@@ -1,19 +1,24 @@
 ﻿using Client.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Client.Services
 {
     public class ReviewService : IReviewService
     {
+        private const string BaseUrl = "https://api20191027115858.azurewebsites.net/api/";
+
         public async Task<object> GetReviews()
         {
-            throw new NotImplementedException();
+            var client = new HttpClient();
+            return await client.GetAsync<List<Review>>(BaseUrl + "Review");
         }
 
         public async Task<object> GetReview(long? id)
         {
-            throw new NotImplementedException();
+            var client = new HttpClient();
+            return await client.GetAsync<Review>(BaseUrl + "Review/" + id);
         }
 
         public void Add(Review review)
